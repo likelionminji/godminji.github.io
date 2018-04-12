e2fa6fb3144ec3caeed9214a880e7fbeb90e3785
Min Ji Han <gksalswl1229@likelion.org>
2018-04-12 08:37:02 +0000

    2차

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,15 +6,12 @@ class HomeController < ApplicationController
     
     def result
         @results=[ "착함", "순수함", "이쁨", "귀여움", "나쁨", "멍청함", "재력", "지력"]
-        @results.sample(3)
-        result.each
-    
-        num = 1..10
-        num = num.to_a
-        @spoon = num.to_a.sample(3).sort
+        @results=@results.sample(3)
+        @spoon=(1..10).to_a.sample(3)
         
+
         
         @name=params[:username]
     end
+ end
 
-end
